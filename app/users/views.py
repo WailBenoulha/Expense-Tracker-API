96148c0f537d7d5d59d7505c5be15a25a670f99b
WailBenoulha <wailbnlh@gmail.com>
2024-11-25 14:22:00 -1100

    fix the permission of register

--- app/users/views.py
+++ app/users/views.py
@@ -1,18 +1,30 @@
 from rest_framework import generics
 from .serializers import UserSerializer
 from core.models import CustomUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+
 
 class CreateUserView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = []
+    permission_classes = [AllowAny]
 
 class UpdateUserView(generics.UpdateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer 
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class DeleteUserView(generics.DestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer  
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class getUserView(generics.ListAPIView):
     queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer         
+    serializer_class = UserSerializer 
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAdminUser]        
